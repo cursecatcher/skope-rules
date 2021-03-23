a19659aa1e8b41c805c2d17dcc5b84455ace45e9
Nicola <nicola.licheri@gmail.com>
2021-03-23 10:37:38 +0100

    [add] - dual skoper class

--- skrules/__init__.py
+++ skrules/__init__.py
@@ -1,4 +1,5 @@
 from .skope_rules import SkopeRules
 from .rule import Rule, replace_feature_name
+from .dual_skope import DualSkoper
 
-__all__ = ['SkopeRules', 'Rule']
+__all__ = ['SkopeRules', 'Rule', "DualSkoper"]
